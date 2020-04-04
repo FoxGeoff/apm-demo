69ac42595657d9157af3988c4486af16a6a9ff1e
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-04-03 17:37:59 -0700

    Build a Child Filter Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { StarComponent } from './star.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   imports: [
@@ -10,12 +11,14 @@ import { StarComponent } from './star.component';
     FormsModule
   ],
   declarations: [
-    StarComponent
+    StarComponent,
+    FilterComponent
   ],
   exports: [
     StarComponent,
     CommonModule,
-    FormsModule
+    FormsModule,
+    FilterComponent
   ]
 })
 export class SharedModule { }
